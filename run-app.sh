d839af710274252a1535587f7ccd0ba7d063b476
flatsiedatsie <unfold@gmail.com>
2022-12-08 23:19:24 +0100

    Attempt to run node 16 (#87)

--- run-app.sh
+++ run-app.sh
@@ -1,71 +1,14 @@
 #!/bin/bash
-
-set -e -x
-
+echo "here we go"
+#set -e -x
+NVM_DIR="/home/pi/.nvm"
 WEBTHINGS_HOME="${WEBTHINGS_HOME:=${HOME}/.webthings}"
-args=""
-start_task="run-only"
+_node_version=$(node --version | egrep -o '[0-9]+' | head -n1)
 
-is_container() {
-  if [ -f /.dockerenv ]; then
-    return 0
-  fi
 
-  if [ -f /run/.containerenv ]; then
-    return 0
-  fi
+echo $NVM_DIR
+source $NVM_DIR/nvm.sh
 
-  if grep -q ':\/docker\/' /proc/1/cgroup 2>&1; then
-    return 0
-  fi
-
-  if [ -d /pantavisor ]; then
-    return 0
-  fi
-
-  return 1
-}
-
-run_app() {
-  # load nvm
-  export NVM_DIR="${HOME}/.nvm"
-  [ ! -s "${NVM_DIR}/nvm.sh" ] || \. "${NVM_DIR}/nvm.sh"
-
-  if [ ! is_container ]; then
-    sudo /sbin/ldconfig
-
-    (sudo timedatectl set-local-rtc 0 && sudo timedatectl set-ntp 1) || true
-  fi
-
-  echo "node version"
-  node --version
-  echo "npm version"
-  npm --version
-  echo "Starting gateway ..."
-  npm run "${start_task}" -- $args
-}
-
-if ! is_container; then
-  if [ ! -f .post_upgrade_complete ]; then
-    ./tools/post-upgrade.sh
-  fi
-else
-  _node_version=$(node --version | egrep -o '[0-9]+' | head -n1)
-  if [[ ! -f "${WEBTHINGS_HOME}/.node_version" ||
-        "$(< "${WEBTHINGS_HOME}/.node_version")" != "${_node_version}" ]]; then
-    cd "${HOME}/webthings/gateway"
-    mkdir -p "${WEBTHINGS_HOME}/config"
-    ./tools/update-addons.sh
-    cd -
-    echo "${_node_version}" > "${WEBTHINGS_HOME}/.node_version"
-  fi
-fi
-
-if [[ "$WEBTHINGS_HOME" == "$HOME/.webthings" &&
-      -d "$HOME/.mozilla-iot" &&
-      ! -e "$HOME/.webthings" ]]; then
-  mv "$HOME/.mozilla-iot" "$HOME/.webthings"
-fi
 
 mkdir -p "${WEBTHINGS_HOME}/log"
 
@@ -73,4 +16,46 @@ if [ -f "${WEBTHINGS_HOME}/log/run-app.log" ]; then
   rm -f "${WEBTHINGS_HOME}/log/run-app.log"
 fi
 
-run_app
+
+# If a new main version of Node is being used, update all the addons
+if [[ ! -f "${WEBTHINGS_HOME}/.node_version" ||
+        "$(< "${WEBTHINGS_HOME}/.node_version")" != "${_node_version}" ]]; then
+  cd "${HOME}/webthings/gateway"
+  mkdir -p "${WEBTHINGS_HOME}/config"
+  ./tools/update-addons.sh
+  cd -
+  echo "${_node_version}" > "${WEBTHINGS_HOME}/.node_version"
+fi
+
+
+if [ -f /boot/candle_node12.txt ]; then
+  if [[ _node_version != "12" ]]; then
+    nvm use 12
+    nvm alias default 12
+    npm rebuild
+
+    cd "${HOME}/webthings/gateway"
+    mkdir -p "${WEBTHINGS_HOME}/config"
+    ./tools/update-addons.sh
+    cd -
+    echo "12" > "${WEBTHINGS_HOME}/.node_version"
+  fi
+
+  nvm exec 12 node --version
+  nvm exec 12 node build/app.js
+else
+  if [[ _node_version != "16" ]]; then
+    nvm use 16
+    nvm alias default 16
+    npm rebuild
+
+    cd "${HOME}/webthings/gateway"
+    mkdir -p "${WEBTHINGS_HOME}/config"
+    ./tools/update-addons.sh
+    cd -
+    echo "16" > "${WEBTHINGS_HOME}/.node_version"
+  fi
+
+  nvm exec 16 node --version
+  nvm exec 16 node build/app.js
+fi
