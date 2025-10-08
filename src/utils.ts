edf36698993c734eee740371698346a37db70395
flatsiedatsie <unfold@gmail.com>
2025-10-08 13:41:25 +0200

    Add files via upload

--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,8 @@ import crypto from 'crypto';
 import fs from 'fs';
 import * as Platform from './platform';
 import pkg from './package.json';
+import { Event as EventSchema } from 'gateway-addon/lib/schema';
+import * as Constants from './constants';
 
 /**
  * Compute a SHA-256 checksum of a file.
@@ -74,17 +76,108 @@ export function isW3CThingDescription(thingDescription: {
 }): boolean {
   if (typeof thingDescription['@context'] === 'string') {
     return (
-      thingDescription['@context'] === 'https://www.w3.org/2019/wot/td/v1' ||
-      thingDescription['@context'] === 'http://www.w3.org/ns/td'
+      thingDescription['@context'] === Constants.WOT_TD_1_CONTEXT ||
+      thingDescription['@context'] === Constants.WOT_TD_1_1_CONTEXT ||
+      thingDescription['@context'] === Constants.WOT_TD_NS_CONTEXT
     );
   }
 
   if (Array.isArray(thingDescription['@context'])) {
     return (
-      thingDescription['@context'].includes('https://www.w3.org/2019/wot/td/v1') ||
-      thingDescription['@context'].includes('http://www.w3.org/ns/td')
+      thingDescription['@context'].includes(Constants.WOT_TD_1_CONTEXT) ||
+      thingDescription['@context'].includes(Constants.WOT_TD_1_1_CONTEXT) ||
+      thingDescription['@context'].includes(Constants.WOT_TD_NS_CONTEXT)
     );
   }
 
   return false;
+}
+
+/**
+ * Make sure the @context value of a Thing Description contains both
+ * W3C WoT TD 1.1 and WebThings contexts.
+ *
+ * @param context {(string|string[])} The non-standardised context value.
+ * @returns string[] The standardised context array.
+ */
+export function standardizeContext(context: string | string[]): string[] {
+  // If expected contexts already present then return null
+  if (
+    Array.isArray(context) &&
+    context.includes(Constants.WOT_TD_1_1_CONTEXT) &&
+    context.includes(Constants.WEBTHINGS_CONTEXT)
+  ) {
+    return context;
+  }
+
+  // If @context is a string, make it an array
+  if (typeof context === 'string') {
+    context = [context];
+  }
+
+  // Remove legacy iot.mozilla.org @context
+  if (context.includes(Constants.MOZ_IOT_CONTEXT)) {
+    context.splice(context.indexOf(Constants.MOZ_IOT_CONTEXT));
+  }
+
+  // If @context doesn't contain W3C TD 1.1 context then add it to the start
+  if (!context.includes(Constants.WOT_TD_1_1_CONTEXT)) {
+    context.unshift(Constants.WOT_TD_1_1_CONTEXT);
+  }
+
+  // If @context doesn't contain WebThings context then add it to the end
+  if (!context.includes(Constants.WEBTHINGS_CONTEXT)) {
+    context.push(Constants.WEBTHINGS_CONTEXT);
+  }
+
+  return context;
+}
+
+/**
+ * Convert event descriptions from the legacy format in the Web Thing API
+ * (https://webthings.io/api/#event-object) to the W3C WoT TD standard format
+ * (https://w3c.github.io/wot-thing-description/#eventaffordance).
+ *
+ * @param {EventSchema} description
+ * @returns {EventSchema}
+ */
+export function standardizeEventDescription(description: EventSchema): EventSchema {
+  if (description.hasOwnProperty('href')) {
+    delete description.href;
+    console.warn('The href member of event descriptions is deprecated');
+  }
+  description.data = description.data || {};
+  if (description.hasOwnProperty('type')) {
+    description.data.type = description.type;
+    delete description.type;
+    console.warn('The type member of event descriptions is deprecated, please use data.type');
+  }
+  if (description.hasOwnProperty('unit')) {
+    description.data.unit = description.unit;
+    delete description.unit;
+    console.warn('The unit member of event descriptions is deprecated, please use data.unit');
+  }
+  if (description.hasOwnProperty('minimum')) {
+    description.data.minimum = description.minimum;
+    delete description.minimum;
+    console.warn('The minimum member of event descriptions is deprecated, please use data.minimum');
+  }
+  if (description.hasOwnProperty('maximum')) {
+    description.data.maximum = description.maximum;
+    delete description.maximum;
+    console.warn('The maximum member of event descriptions is deprecated, please use data.maximum');
+  }
+  if (description.hasOwnProperty('multipleOf')) {
+    description.data.multipleOf = description.multipleOf;
+    delete description.multipleOf;
+    console.warn(
+      'The multipleOf member of event descriptions is deprecated, please use data.multipleOf'
+    );
+  }
+  if (description.hasOwnProperty('enum')) {
+    description.data.enum = description.enum;
+    delete description.enum;
+    console.warn('The enum member of event descriptions is deprecated, please use data.enum');
+  }
+  return description;
 }
