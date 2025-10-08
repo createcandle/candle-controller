e9fee18db0e58c1080df6fe6f8f71090a7643d1e
flatsiedatsie <unfold@gmail.com>
2025-10-08 12:03:10 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -383,6 +383,9 @@ function startGateway(): void {
 }
 
 function gracefulExit(): void {
+  if (Platform.implemented('stop')) {
+    Platform.stop();
+  }
   AddonManager.unloadAddons();
   TunnelService.stop();
 }
