b53268414d8b98d83d5cdc96043a8cba8e8ba7be
flatsiedatsie <unfold@gmail.com>
2025-10-13 16:52:47 +0200

    Add files via upload

--- src/controllers/events_controller.ts
+++ src/controllers/events_controller.ts
@@ -8,7 +8,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import express from 'express';
+import express, { Request, Response } from 'express';
 import Things from '../models/things';
 import Thing from '../models/thing';
 import Events from '../models/events';
@@ -20,7 +20,7 @@ function build(): express.Router {
   /**
    * Handle getting events of all types.
    */
-  controller.get('/', (request, response) => {
+  controller.get('/', (request: Request, response: Response) => {
     // Serve either an event stream or a log depending on requested content type
     if (request.accepts('text/event-stream')) {
       openEventStream(request, response);
@@ -32,7 +32,7 @@ function build(): express.Router {
   /**
    * Handle getting events of a specific type.
    */
-  controller.get('/:eventName', (request, response) => {
+  controller.get('/:eventName', (request: Request, response: Response) => {
     // Serve either an event stream or a log depending on requested content type
     if (request.accepts('text/event-stream')) {
       openEventStream(request, response);
@@ -106,8 +106,8 @@ function build(): express.Router {
         return;
       }
 
-      // Generate an ID for the event
-      const eventId = Date.now();
+      // Generate an ISO 8601 date time string as an ID
+      const eventId = new Date().toISOString();
 
       // Push event to client via event stream
       response.write(`id: ${eventId}\n`);
--- src/controllers/things_controller.ts
+++ src/controllers/things_controller.ts
@@ -8,6 +8,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+import PropertiesController from './properties_controller';
 import Action, { ActionDescription } from '../models/action';
 import Actions from '../models/actions';
 import ActionsController from './actions_controller';
@@ -216,8 +217,6 @@ function build(): express.Router {
     const id = description.id;
     delete description.id;
 
-    console.log("thing_controller.ts: creating a new thing (hopefully): ", id, description);
-    
     try {
       // If the thing already exists, bail out.
       await Things.getThing(id);
@@ -230,10 +229,6 @@ function build(): express.Router {
     }
     const isWotAdapterInstalled = AddonManager.isAddonInstalled('wot-adapter');
     const isThingUrlInstalled = AddonManager.isAddonInstalled('thing-url-adapter');
-
-    console.log("isWotAdapterInstalled: ", isWotAdapterInstalled);
-    console.log("isThingUrlInstalled: ", isThingUrlInstalled);
-    
     // If we're adding a native webthing, we need to update the config for
     // thing-url-adapter or wot-adpater so that it knows about it.
     let webthing = false;
@@ -287,7 +282,6 @@ function build(): express.Router {
    */
   controller.get('/:thingId', (request, response) => {
     const id = request.params.thingId;
-    console.log("thing_controller.ts: get a thing.  id, host, secure: ", id, request.get('Host'), request.secure);
     Things.getThingDescription(id, request.get('Host'), request.secure)
       .then((thing) => {
         response.status(200).json(thing);
@@ -299,94 +293,9 @@ function build(): express.Router {
   });
 
   /**
-   * Get the properties of a Thing.
+   * Use a PropertiesController to handle each thing's properties.
    */
-  controller.get('/:thingId/properties', async (request, response) => {
-    const thingId = request.params.thingId;
-
-    let thing;
-    try {
-      thing = await Things.getThing(thingId);
-    } catch (e) {
-      console.error('Failed to get thing:', e);
-      response.status(404).send(e);
-      return;
-    }
-
-    console.log("thing_controller.ts: get properties of a thing (hopefully).  thingId,: ", thingId, thing);
-    
-    const result: Record<string, Any> = {};
-    for (const name in thing.getProperties()) {
-      try {
-        const value = await AddonManager.getProperty(thingId, name);
-        result[name] = value;
-      } catch (e) {
-        console.error(`Failed to get property ${name}:`, e);
-      }
-    }
-
-    response.status(200).json(result);
-  });
-
-  /**
-   * Set multiple properties of a Thing.
-   */
-  controller.put('/:thingId/properties', async (request, response) => {
-    const thingId = request.params.thingId;
-    if (!(typeof request.body === 'object') || request.body === null) {
-      response.sendStatus(400);
-      return;
-    }
-    // An array of promises to set each property
-    const promises = [];
-    for (const propertyName of Object.keys(request.body)) {
-      promises.push(Things.setThingProperty(thingId, propertyName, request.body[propertyName]));
-    }
-    Promise.all(promises)
-      .then(() => {
-        // Respond with success code if all properties set successfully
-        response.sendStatus(204);
-      })
-      .catch((err) => {
-        // Otherwise send an error response
-        console.error('Error setting property:', err);
-        response.sendStatus(500);
-      });
-  });
-
-  /**
-   * Get a property of a Thing.
-   */
-  controller.get('/:thingId/properties/:propertyName', async (request, response) => {
-    const thingId = request.params.thingId;
-    const propertyName = request.params.propertyName;
-    try {
-      const value = await Things.getThingProperty(thingId, propertyName);
-      response.status(200).json(value);
-    } catch (err) {
-      response.status(err.code).send(err.message);
-    }
-  });
-
-  /**
-   * Set a property of a Thing.
-   */
-  controller.put('/:thingId/properties/:propertyName', async (request, response) => {
-    const thingId = request.params.thingId;
-    const propertyName = request.params.propertyName;
-    if (typeof request.body === 'undefined') {
-      response.sendStatus(400);
-      return;
-    }
-    const value = request.body;
-    try {
-      await Things.setThingProperty(thingId, propertyName, value);
-      response.sendStatus(204);
-    } catch (err) {
-      console.error('Error setting property:', err);
-      response.status(err.code || 500).send(err.message);
-    }
-  });
+  controller.use(`/:thingId${Constants.PROPERTIES_PATH}`, PropertiesController());
 
   /**
    * Use an ActionsController to handle each thing's actions.
@@ -648,7 +557,6 @@ function build(): express.Router {
 
       // send initial property values
       for (const name in thing.getProperties()) {
-        console.log("things_controller.ts: asking for thing property from AddonManager: ", thing.getId(), name);
         AddonManager.getProperty(thing.getId(), name)
           .then((value) => {
             sendMessage({
