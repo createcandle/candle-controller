58a727d0a0597fce0426d0c9b5a6c215cde5de0a
flatsiedatsie <unfold@gmail.com>
2026-02-02 15:04:19 +0100

    Update make-self-signed-cert.sh

--- tools/make-self-signed-cert.sh
+++ tools/make-self-signed-cert.sh
@@ -8,4 +8,4 @@ SSL_DIR="${WEBTHINGS_HOME}/ssl"
 openssl genrsa -out "${SSL_DIR}/privatekey.pem" 2048
 #openssl req -new -sha256 -key "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/csr.pem" -subj '/CN=www.sgnihtbew.com/O=WebThings Gateway/C=US'
 openssl req -new -sha256 -key "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/csr.pem" -subj '/CN=www.sgnihtbew.com/O=Candle Controller/C=US'
-openssl x509 -req -in "${SSL_DIR}/csr.pem" -signkey "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/certificate.pem"
+openssl x509 -req -days 3650 -in "${SSL_DIR}/csr.pem" -signkey "${SSL_DIR}/privatekey.pem" -out "${SSL_DIR}/certificate.pem"
