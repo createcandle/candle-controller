cf23a7e51f0e54f110fc82858d4cc75b730996b6
flatsiedatsie <unfold@gmail.com>
2026-04-03 17:57:39 +0200

    Update thing-model.js

--- static/js/models/thing-model.js
+++ static/js/models/thing-model.js
@@ -131,6 +131,22 @@ class ThingModel extends Model {
       switch (message.messageType) {
         case 'propertyStatus':
           this.onPropertyStatus(message.data);
+          if(typeof window.snoopers != 'undefined' && 
+             typeof this.id == 'string' && 
+             typeof window.snoopers[this.id] == 'object' &&
+             Array.isArray(window.snoopers[this.id])
+          ){
+            for(let f = 0; f < window.snoopers[this.id].length; f++){
+              if (typeof window.snoopers[this.id][f] === 'function') {
+                try{
+                  window.snoopers[this.id][f](message.data);
+                }
+                catch(err){
+                  console.error("thing-model: caught error passing message to snooper for this.id: ", this.id, err);
+                }
+              }
+            }
+          }
           break;
         case 'event':
           this.onEvent(message.data);
