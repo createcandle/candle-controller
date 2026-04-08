5a54f8dec1c6bd254ccbe7e3b3a23fecf427d73a
flatsiedatsie <unfold@gmail.com>
2026-04-08 12:55:03 +0200

    Update linux-raspbian.ts

--- src/platforms/linux-raspbian.ts
+++ src/platforms/linux-raspbian.ts
@@ -1208,7 +1208,7 @@ export class LinuxRaspbianPlatform extends BasePlatform {
         if(wifiIp4Config.length == 0){
           continue
         }
-        if (wifiIp4Config[0].address === '192.168.12.1') {
+        if (wifiIp4Config[0].address.startsWith('172.16.') && wifiIp4Config[0].address.endsWith('.1')) {
           // Skip the hotspot on the uap0 interface
           continue
         }
@@ -1382,7 +1382,7 @@ export class LinuxRaspbianPlatform extends BasePlatform {
     let wifiDevice = null;
     for (let wd = 0; wd < wifiDevices.length; wd++) {
       const wifiIp4Config = await NetworkManager.getDeviceIp4Config(wifiDevices[wd]);
-      if (Array.isArray(wifiIp4Config) && wifiIp4Config.length && wifiIp4Config[0].address === '192.168.12.1'){
+      if (Array.isArray(wifiIp4Config) && wifiIp4Config.length && wifiIp4Config[0].address.startsWith('172.16.') && wifiIp4Config[0].address.endsWith('.1')){
         continue
       }
       wifiDevice = wifiDevices[wd];
@@ -1463,7 +1463,7 @@ export class LinuxRaspbianPlatform extends BasePlatform {
     let wifiDevice = null;
     for (let wd = 0; wd < wifiDevices.length; wd++) {
       const wifiIp4Config = await NetworkManager.getDeviceIp4Config(wifiDevices[wd]);
-      if (Array.isArray(wifiIp4Config) && wifiIp4Config.length && wifiIp4Config[0].address === '192.168.12.1'){
+      if (Array.isArray(wifiIp4Config) && wifiIp4Config.length && wifiIp4Config[0].address.startsWith('172.16.') && wifiIp4Config[0].address.endsWith('.1')){
         continue
       }
       wifiDevice = wifiDevices[wd];
