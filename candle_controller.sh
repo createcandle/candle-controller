89ec73ffe5e6c8434717ec5349347577ba964b47
flatsiedatsie <unfold@gmail.com>
2026-04-19 16:54:36 +0200

    Update candle_controller.sh

--- candle_controller.sh
+++ candle_controller.sh
@@ -56,7 +56,7 @@ fi
 #DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 #export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-
+export DBUS_SESSION_BUS_ADDRESS="$XDG_RUNTIME_DIR/bus"
 
 #echo "AFTER:"
 echo "XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR"
